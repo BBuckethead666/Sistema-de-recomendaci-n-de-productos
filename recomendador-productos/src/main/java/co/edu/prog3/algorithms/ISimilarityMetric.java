7b25ae12ec9ce119ac17978138ea075c4d7c4324
BBuckethead666 <diegomesaduart@gmail.com>
2025-11-17 19:16:36 -0500

    carpetas inescesarias eliminadas

--- recomendador-productos/src/main/java/co/edu/prog3/algorithms/ISimilarityMetric.java
+++
@@ -1,8 +0,0 @@
-package co.edu.prog3.algorithms;
-
-import co.edu.prog3.model.Product;
-
-public interface ISimilarityMetric {
-    String name();
-    double similarity(Product a, Product b); // rango esperado [0, 1]
-}
